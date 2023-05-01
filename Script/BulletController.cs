f03cfb2c86fea434e222b84f63f3c291efbf30cf
unknown <clsrns11111@naver.com>
2023-05-01 22:10:40 +0900

    연속 공격기능

--- Script/BulletController.cs
+++ Script/BulletController.cs
@@ -6,12 +6,13 @@ public class BulletController : MonoBehaviour
 {
    private Vector3 direction; 
    private float speed; 
-
    private Vector3 enemyPosition;
+
+   private int count = 0;
 
    private void Update() 
    {
-        transform.position += direction * speed * Time.deltaTime;
+        transform.position += this.direction * speed * Time.deltaTime;
    }
 
     public void SetDirectionAndSpeed(Vector3 direction, float speed)
@@ -31,23 +32,39 @@ public class BulletController : MonoBehaviour
             GameObject otherGameObject= other.gameObject;
             Enemy enmey = otherGameObject.GetComponent<Enemy>();
             enmey.Hp = 0; 
-            Lighting();
+            count++; 
+
+            Vector2 direction = (other.transform.position - transform.position).normalized; // 충돌한 객체와 총알의 위치 벡터 차를 구하고 정규화하여 방향 벡터 계산
+            this.direction = direction;
+            transform.Translate(direction * speed * Time.deltaTime); // 총알 이동 방향을 변경하여 적 방향으로 날아가도록 함
+
+            if(count >= 10)
+                Destroy(gameObject);
+            // Lighting();
             // Destroy(gameObject);
         } 
     }
 
     private void Lighting()
     {
-        // GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
-        float detectionRadius = 10f; 
+        Vector3 hitPos = transform.position;
 
-        Collider[] colliders = Physics.OverlapSphere(transform.position, detectionRadius, LayerMask.GetMask("Enmey")); 
+        Collider[] colliders = Physics.OverlapSphere(hitPos, 2f, LayerMask.GetMask("Enemy")); 
 
-        foreach(Collider collider in colliders)
+        foreach (var collider in colliders)
         {
-            GameObject enemy = collider.gameObject;
-            Debug.Log("enemy: " + enemy.transform.position);
+            Vector3 enemyPos = collider.transform.position;
+
+            Vector3 diff = enemyPos - hitPos;
+            diff.y = 0f; 
+
+            if(diff != Vector3.zero)
+            {
+                transform.forward = diff.normalized;
+                break;
+            }
         }
+        // HashUtilities = true;
 
     }
 
